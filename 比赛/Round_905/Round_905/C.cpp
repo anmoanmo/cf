f48750f478fe6efa0e5eabc196f8f2b930e17f12
anmo <anmoanmomo@gmail.com>
2023-10-23 22:10:18 +0800

    2023/10/23 22：10

--- 比赛/Round_905/Round_905/C.cpp
+++ 比赛/Round_905/Round_905/C.cpp
@@ -16,110 +16,84 @@
 //
 //using namespace std;
 //
+//
 //int main() {
-//	ios::sync_with_stdio(false);
-//	cin.tie(NULL);
-//	cout.tie(NULL);
+//    ios::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
 //
-//	int t = 0; cin >> t;
-//	int n, k;
-//	while (t--) {
-//		cin >> n >> k;
-//		vector<int> v(n);
-//		int op = 1e5;  // 记录操作次数
+//    int t = 0; cin >> t;
+//    int n, k;
+//    int flg = 0, flg1 = 0, flg2 = 0;
+//    while (t--) {
+//        flg1 = 0;
+//        flg2 = 0;
+//        cin >> n >> k;
+//        vector<int> v(n);
+//        int op = 1e5;  // 记录操作次数
 //
-//		for (int i = 0; i < n; i++) {
-//			cin >> v[i];
-//		}
-//		int sum = 1;
-//		for (int i = 0; i < n; i++) {
-//			sum *= v[i];
-//		}
-//		if (sum % k == 0) {
-//			cout << 0 << endl;
-//			continue;
-//		}
-//		for (int i = 0; i < n; i++) {
-//			int flag = 0;
-//			while (v[i] % k != 0) {
-//				v[i]++;
-//				if (v[i] > 10) {
-//					break;
-//				}
-//				flag++;
-//			}
-//			op = min(flag, op);  // 更新最小操作次数
-//		}
+//        for (int i = 0; i < n; i++) {
+//            cin >> v[i];
+//        }
 //
-//		cout << op << endl;
-//	}
-//	return 0;
+//        // 检查整个数组的乘积是否可以被k整除
+//        ll sum = 1;
+//        for (int i = 0; i < n; i++) {
+//            sum = (sum * v[i]) % k; // 对k取模，避免溢出
+//            if (sum == 0) {
+//                break;
+//            }
+//        }
+//        if (sum == 0) {
+//            cout << 0 << endl;
+//            continue;
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            int temp = v[i];
+//
+//            if (k == 4) {
+//                flg1 = 1;
+//                flg2 = 0;
+//                flg = 0;
+//                for (int j = 0; j < n; j++) {
+//
+//                    if ((v[j] + 1) % 4 == 0) {
+//                        flg2 = 1;
+//                        cout << 1 << endl;
+//                        break;
+//                    }
+//                }
+//                for (int j = 0; j < n; j++) {
+//
+//                    if ((v[j]) % 2 == 0) {
+//                        flg++;
+//                    }
+//                }
+//                break;
+//            }
+//
+//            int flag = 0;
+//            while (v[i] % k) {
+//                v[i]++;
+//                flag++;
+//            }
+//            op = min(flag, op);  // 更新最小操作次数
+//        }if (!flg2) {
+//            if (flg1) {
+//                if (flg % 2 == 1) {
+//                    cout << 1 << endl;
+//                    continue;
+//                }
+//                else {
+//                    cout << 2 << endl;
+//                    continue;
+//                }
+//            }
+//            cout << op << endl;
+//        }
+//    }
+//    return 0;
 //}
-#define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include<cmath>
-#include<string>
-#include<vector>
-#include<list>
-#include<queue>
-#include<deque>
-#include<stack>
-#include<map>
-#include<algorithm>
-#include<iomanip>
-typedef long long ll;
-
-#define endl '\n'
-
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t = 0; cin >> t;
-    int n, k;
-    while (t--) {
-        cin >> n >> k;
-        vector<int> v(n);
-        int op = 1e5;  // 记录操作次数
-
-        for (int i = 0; i < n; i++) {
-            cin >> v[i];
-        }
-
-        // 检查整个数组的乘积是否可以被k整除
-        ll prod = 1;
-        for (int i = 0; i < n; i++) {
-            prod *= v[i];
-        }
-        if (prod % k == 0) {
-            cout << 0 << endl;
-            continue;
-        }
-
-        for (int i = 0; i < n; i++) {
-            int temp = v[i];
-            int flag = 0;
-            while (true) {
-                ll newProd = (prod / v[i]) * temp;
-                if (newProd % k == 0) {
-                    break;
-                }
-                temp++;
-                flag++;
-                if (temp > 10) {  // 设置上限以避免无限循环
-                    flag = 1e5; // 设置一个大值
-                    break;
-                }
-            }
-            op = min(flag, op);  // 更新最小操作次数
-        }
-
-        cout << op << endl;
-    }
-    return 0;
-}
 
 
